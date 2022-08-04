9507793ce054afd20f7c1dff1921756df92f7ca3
orefakolujo <orefakolujo@gmail.com>
2022-08-04 04:27:16 +0100

    Updated Urls and Views

--- termsgen/urls.py
+++ termsgen/urls.py
@@ -1,7 +1,14 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("products/", views.products)
+    path("", views.index, name="home"),
+    path("products/", views.products, name="products"),
+    path("contact-us/", views.contact_us, name="contact_us"),
+    path("about-us/", views.about_us, name="about_us"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("basic-info/", views.basic_info, name="basic_info"),
+    path("web-info/", views.web_info),
+    path("privacy/", views.privacy)
 ]
