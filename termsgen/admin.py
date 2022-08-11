c239807df107cbd3885221c4e736458be64b8d31
Oreoluwa Fakolujo.com <orefakolujo@gmail.com>
2022-08-11 18:16:15 +0100

    latest

--- termsgen/admin.py
+++ termsgen/admin.py
@@ -1,6 +1,8 @@
-import imp
-from multiprocessing.spawn import import_main_path
-from typing import OrderedDict
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import database
+
+
+admin.site.register(database)
